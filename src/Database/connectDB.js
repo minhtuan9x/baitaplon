f88029d98370d54551f48a55661d749698d57a97
Minhtuan9x <0mtmtmt0@gmail.com>
2021-11-07 22:23:44 +0700

    tuan

--- src/Database/connectDB.js
+++ src/Database/connectDB.js
@@ -1,10 +1,19 @@
-const mongoose = require("mongoose");
-var url = 'mongodb://localhost:27017/quanlibatdongsan';
-function connectDB() {
-    try {
-        mongoose.connect(url);
-    } catch (error) {
-        console.log(error);
-    }
+
+const mongoose = require('mongoose')
+
+const url = 'mongodb+srv://root:admin@cluster0.gsrso.mongodb.net/batdongsan?retryWrites=true&w=majority';
+
+const connectionParams={
+    useNewUrlParser: true, useUnifiedTopology: true   
 }
+function connectDB(){
+mongoose.connect(url,connectionParams)
+    .then( () => {
+        console.log('Connected to database ')
+    })
+    .catch( (err) => {
+        console.error(`Error connecting to the database. \n${err}`);
+    })
+}
+
 module.exports = connectDB;

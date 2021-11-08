82ac4c69dcd6013f4d6a6732860a83c928cdbe77
Minhtuan9x <0mtmtmt0@gmail.com>
2021-11-08 18:48:22 +0700

    tuan

--- src/models/building.js
+++ src/models/building.js
@@ -1,14 +1,23 @@
 const mongoose = require("mongoose");
 var buildingSchema = mongoose.Schema({
-    id:Number,
     name:String,
-    address:String,
+    street:String,
+    ward:String,
+    districtid:Number,
     rentprice:String,
     rentarea:String,
-    level:Number,
     electricfee:String,
     waterfee:String,
-    imagelink:String
+    imagelink:String,
+    managerphone:String,
+    managername:String,
+    sellprice:String,
+    mortobikefee:String,
+    carfee:String,
+    renttypeids:Array,
+    directory:String,
+    note:String
+
 })
 var building = mongoose.model("building",buildingSchema);
 module.exports  = building;

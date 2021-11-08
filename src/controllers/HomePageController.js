94e807bcb10147b70d5c5a5123376786f94b022c
Minhtuan9x <0mtmtmt0@gmail.com>
2021-11-08 18:33:33 +0700

    tuan

--- src/controllers/HomePageController.js
+++ src/controllers/HomePageController.js
@@ -3,7 +3,7 @@ class HomePageController{
 
     //[GET] / (HOME PAGE)
     index(req, res){
-        res.render('home/index');
+        res.render('./home/index');
        // res.send('HOME PAGE')
     }
 

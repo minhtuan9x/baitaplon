6d34d1f8f33b1498bcc281ff6118329235151cec
NguyenTrungThanh123 <93855612+NguyenTrungThanh123@users.noreply.github.com>
2021-11-09 20:35:28 +0700

    thanh

--- src/app.js
+++ src/app.js
@@ -3,12 +3,20 @@ const app = express();
 const route = require('./routes/main');
 const ejs = require('ejs');
 const path = require('path');
+const methodOverride = require('method-override');
 
 
 
 const port = 3000
 var db = require("./database/connectDB");
 db();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use(methodOverride('_method'));
+
+
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname,'views'));

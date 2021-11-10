89140cf76d3cddd0571eba23acb338028548a73b
NguyenTrungThanh123 <93855612+NguyenTrungThanh123@users.noreply.github.com>
2021-11-10 10:38:17 +0700

    thanh

--- src/controllers/BuildingController.js
+++ src/controllers/BuildingController.js
@@ -1,52 +1,8 @@
 const building = require('../models/building');
 const district = require('../models/district');
 const renttype = require('../models/renttype');
-class BuildingController {
 
-<<<<<<< HEAD
 class BuildingController{
-//[GET] /building  
-index(req,res){
-    res.render('building/indexBuilding');
-}
-//[GET] buidling/insert  
-insertView(req,res){
-    res.render('building/addBuilding')
-}
-//[GET] building/:id/update  
-updateView(req,res){
-    var id = req.params.id;
-    building.findOne({_id:id},function(err,data){
-        if(err) 
-            res.send(err);
-        else
-            //res.render('/editBuilding',{item : data});
-            res.send(data);
-    })
-}
-//[GET] /search/:nameBuilding
-searchName(req,res){
-    var nameBuilding = req.query.nameBuilding.toString(); 
-    building.find({name:nameBuilding},function(err,data){
-        res.render('buidling/searchBuilding',{list:data});
-    });
-}
-//[POST] /building/insert
-insertModel(req, res){
-    var dataInsert = req.body;
-    var dataBuilding = {
-        "name":dataInsert.nameBuilding,
-        "rentarea":dataInsert.rentareaBuilding,
-        "imagelink":dataInsert.imagelinkBuilding,
-        "street":dataInsert.streetBuilding,
-        "districtid":dataInsert.districtidBuilding,
-        "ward":dataInsert.wardBuilding,
-        "renttypeids":dataInsert.renttypeidsBuilding,
-        "note":dataInsert.noteBuilding,
-        "managername":dataInsert.managernameBuilding,
-        "rentprice":dataInsert.rentpriceBuilding,
-        "sellprice":dataInsert.sellpriceBuilding,
-=======
     //[GET] /building  
     index(req, res) {
         building.find(function (err, data) {
@@ -85,7 +41,32 @@ insertModel(req, res){
                 })
             })
         })
+
     }
+    //[GET] buidling/insert  
+    insertView(req,res){
+        res.render('building/addBuilding')
+    }
+    //[GET] building/:id/update  
+    updateView(req,res){
+        var id = req.params.id;
+        building.findOne({_id:id},function(err,data){
+            if(err) 
+                res.send(err);
+            else
+                //res.render('/editBuilding',{item : data});
+                res.send(data);
+        })
+    }
+    //[GET] /search/:nameBuilding
+    searchName(req,res){
+        var nameBuilding = req.query.nameBuilding.toString(); 
+        building.find({name:nameBuilding},function(err,data){
+            res.render('buidling/searchBuilding',{list:data});
+        });
+    }
+
+    //[GET] /building  
 
     //[GET] buidling/insert  
     insertView(req, res) {
@@ -107,7 +88,6 @@ insertModel(req, res){
         building.find({ name: nameBuilding }, function (err, data) {
             res.render('/buidling/searchBuilding', { list: data });
         });
->>>>>>> d5a88a7ba61e6c1a76f86eeb5cc3bad9d14727eb
     }
     //[POST] /building/insert
     insertModel(req, res) {
@@ -160,8 +140,8 @@ insertModel(req, res){
     //[DELETE] /building/:id/delete
 
 
+
+
 }
-
-
 
 module.exports = new BuildingController;

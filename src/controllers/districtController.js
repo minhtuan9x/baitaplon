0a7e806750b5de7d03911554734636ca9aac7890
NguyenTrungThanh123 <93855612+NguyenTrungThanh123@users.noreply.github.com>
2021-11-11 19:04:06 +0700

    thanh

---
+++ src/controllers/districtController.js
@@ -0,0 +1,82 @@
+const district = require("../models/district.js");
+
+class districtController{
+
+    //[GET] /district
+    index(req,res){
+        district.find(function(err,data){
+            if(err)
+                res.send(err);
+            else{
+                res.render('district/indexDictrict',{list:data});
+                //res.send(data);
+            }
+        })
+    }
+    //[GET] /district/insert
+    insertView(req,res){
+        res.render('district/addDistrict');
+    }
+    //[GET] /district/:id/update
+    updateView(req,res){
+        var idDistrict = req.params.id;
+        var dataDistrict = {
+
+        }
+        district.findById(idDistrict,function(err,data){
+            if(err)
+                res.send(err);
+            else{
+                res.render('district/editDistrict',{item:data});
+            }
+        })
+    }
+    //[GET] /district/search/:nameDistrict
+    search(req,res){
+        var nameDistrict = req.query.nameDistrict.toString();
+        district.findOne({name:district},function(err,data){
+            res.render('district/searchDistrict',{item:data});      
+        })
+    }
+    //[POST] /district/:id/insert
+    insertModel(req,res){
+        var data = req.body;
+        var dataDictrict = {
+            name : data.nameDistrict
+        }
+        district.create(dataDictrict,function(err,data){
+            if(err)
+                res.send(err);
+            else
+                res.redirect('/district');
+        })
+
+    }
+    //[PUT] /district/:id/update
+    updateModel(req,res){
+        var idDistrict = req.params.id;
+        var dataBody = req.body;
+        var dataDistrict = {
+            name: dataBody.nameDistrict
+        }
+        district.updateOne({_id:idDistrict},dataDistrict,function(err,data){
+            if(err)
+                res.send(err);
+            else
+                res.redirect('/district');    
+        })
+    }
+    //[DELETE] /district/:id/delete
+    deleteModel(req,res){
+        var idDictrict = req.params.id;
+        district.deleteOne({_id:id},function(err,data){
+            if(err)
+                res.send(err);
+            else{
+                res.redirect('/district');
+            }
+        })
+    }
+}
+module.exports = new districtController;
+
--- src/routes/district.js
+++ src/routes/district.js
@@ -1,8 +1,21 @@
-const district = require("../controllers/district");
+const district = require("../controllers/districtController");
 const express = require('express');
 const router =express.Router();
 
-router.get('/:id',district.getByDistrictID)
-router.get('/', district.getAll)
+//[GET] /dictrict
+router.get('/', district.index);
+
+//[GET] /dictrict/insert
+router.get('/insert',district.insertView);
+//[GET] /dictrict/:id/update
+router.get('/:id/update',district.updateView);
+//[GET] /district/search/:nameDistrict
+router.get('/search/:id',district.search);
+//[POST] /district/insert
+router.post('/insert',district.insertModel);
+//[PUT] /district/:id/update
+router.put('/:id/update',district.updateModel);
+//[DELETE] /district/:id/delete
+router.delete('/:id/delete',district.deleteModel);
 
 module.exports = router;
